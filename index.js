3aa0dc127ccda16d2d7395621b7b59f7aa938465
lgavlich <lgavlich@gmail.com>
2022-01-22 13:26:37 +0200

    +

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");
-const arr = hideBin(process.argv);
-const { argv } = yargs(arr);
-//const argv = require("yargs").argv;
-const contactOperations = require("./contacts");
+const contactOperations = require("/contacts");
 
-// TODO: рефакторить
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
